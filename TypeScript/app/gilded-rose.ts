1ffb8c8a4c961cfab65ae3056dd4d2b607a392b3
Etienne Lopez <lopezetienne@wanadoo.fr>
2025-08-18 21:36:25 +0200

    Ajout Constante pour éviter la répétition de String Ajout de méthodes pour savoir si un item est aged brie, backstage ou sulfuras Modifications du for pour éviter le i

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,5 +1,15 @@
 import { Item } from "./Item";
 
+
+//mise e, constante pour possible modification future. 1 seul endroit à modifier.
+const NAMES = {
+  AGED_BRIE: "Aged Brie",
+  BACKSTAGE: "Backstage passes to a TAFKAL80ETC concert",
+  SULFURAS: "Sulfuras, Hand of Ragnaros",
+} as const;
+
+const MIN_QUALITY = 0;
+
 export class GildedRose {
   items: Array<Item>;
 
@@ -9,50 +19,50 @@ export class GildedRose {
 
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
+    for (const item of this.items) {
 
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
+      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (item.quality > 0) {
+          if (item.name != 'Sulfuras, Hand of Ragnaros') {
+            item.quality = item.quality - 1
           }
         }
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
+        if (item.quality < 50) {
+          item.quality = item.quality + 1
+          if (this.isBackstage(item)) {
+            if (item.sellIn < 11) {
+              if (item.quality < 50) {
+                item.quality = item.quality + 1
               }
             }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
+            if (item.sellIn < 6) {
+              if (item.quality < 50) {
+                item.quality = item.quality + 1
               }
             }
           }
         }
       }
 
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+      if (item.name != 'Sulfuras, Hand of Ragnaros') {
+        item.sellIn = item.sellIn - 1;
       }
 
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
+      if (item.sellIn < 0) {
+        if (item.name != 'Aged Brie') {
+          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if (item.quality > 0) {
+              if (item.name != 'Sulfuras, Hand of Ragnaros') {
+                item.quality = item.quality - 1
               }
             }
           } else {
-            this.items[i].quality = 0
+            item.quality = 0
           }
         } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
+          if (item.quality < 50) {
+            item.quality = item.quality + 1
           }
         }
       }
@@ -60,7 +70,24 @@ export class GildedRose {
 
     return this.items;
   }
+
+  //Bolean True si l'item esy Aged Brie
+  private isAgedBrie(item: Item): boolean {
+    return item.name === NAMES.AGED_BRIE;
+  }
+
+  //Bolean True si l'item esy BACKSTAGE
+  private isBackstage(item: Item): boolean {
+    return item.name === NAMES.BACKSTAGE;
+  }
+
+  //Bolean True si l'item esy SULFURAS
+  private isSulfuras(item: Item): boolean {
+    return item.name === NAMES.SULFURAS;
+  }
 }
+
+
 
 
 /*
